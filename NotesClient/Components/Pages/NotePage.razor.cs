3018aa23be8deb658458ddf43200b132f9537ff1
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-07-26 23:48:58 +0800

    refactor: Improve NotePage component layout and functionality

--- NotesClient/Components/Pages/NotePage.razor.cs
+++ NotesClient/Components/Pages/NotePage.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using NotesApi.DTOs;
 using NotesClient.Services.Contracts;
 
@@ -14,12 +15,29 @@ public partial class NotePage
 
     private NoteDto? _note;
 
+    [Inject]
+    public NavigationManager Navigation { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         await LoadNote();
     }
 
-    private async Task LoadNote() { 
-        _note = await _service.GetNoteByIdAsync(Id);
+    private async Task LoadNote()
+    {
+        try
+        {
+            _note = await _service.GetNoteByIdAsync(Id);
+        }
+        catch (Exception)
+        {
+            Navigation.NavigateTo("/");
+        }
+    }
+
+    private async Task HandleDeleteNote()
+    {
+        await _service.DeleteNoteAsync(Id);
+        Navigation.NavigateTo("/");
     }
 }
